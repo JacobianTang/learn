2340c9208f48f06dc04aa7a2be146c49a5ff6aa9
qi.tang <qi.tang@ai-baby.com>
2020-07-15 17:38:55 +0800

    python_plt update

--- python_plt/python_diff.py
+++ python_plt/python_diff.py
@@ -5,6 +5,22 @@ def cal_function_value(x):
 
 import matplotlib.pyplot as plt  #导入matplotlib库
 import numpy as np  #导入numpy库
+import mpl_toolkits.axisartist as axisartist
+
+#创建画布
+fig = plt.figure()
+#使用axisartist.Subplot方法创建一个绘图区对象ax
+ax = axisartist.Subplot(fig, 111)
+#将绘图区对象添加到画布中
+fig.add_axes(ax)
+#通过set_axisline_style方法设置绘图区的底部及左侧坐标轴样式
+#"-|>"代表实心箭头："->"代表空心箭头
+ax.axis["bottom"].set_axisline_style("-|>", size = 1.5)
+ax.axis["left"].set_axisline_style("-|>", size = 1.5)
+#通过set_visible方法设置绘图区的顶部及右侧坐标轴隐藏
+ax.axis["top"].set_visible(False)
+ax.axis["right"].set_visible(False)
+
 #生成x步长为0.1的列表数据
 x = np.arange(-15,15,0.1)
 #生成sigmiod形式的y数据
@@ -36,15 +52,13 @@ plt.text(-10,cal_function_value(-10) + cal_function_value(-10), r'$y=f(x)$', fon
 plt.xticks([])
 plt.yticks([])
 
-ax = plt.axes()
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
+# ax.spines['top'].set_visible(False)
+# ax.spines['right'].set_visible(False)
 
-#ax.axis["x"] = ax.new_floating_axis(0,0)
-#ax.axis["x"].set_axisline_style("->", size = 1.0)
-#添加y坐标轴，且加上箭头
-#ax.axis["y"] = ax.new_floating_axis(1,0)
+# ax.axis["x"] = ax.new_floating_axis(0,0)
+# ax.axis["x"].set_axisline_style("->", size = 1.0)
+# 添加y坐标轴，且加上箭头
+# ax.axis["y"] = ax.new_floating_axis(1,0)
 # ax.axis["y"].set_axisline_style("-|>", size = 1.0)
-
 
 plt.show()
